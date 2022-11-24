df5edbef101dc334c515c19b2429bf6fb88e5969
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-24 10:30:36 +0100

    add Repertoire object

--- raptcr/analysis.py
+++ raptcr/analysis.py
@@ -1,3 +1,18 @@
+from itertools import count
+from typing import Iterable, List
+import pandas as pd
+
 class Repertoire():
-    def __init__(self) -> None:
-        pass
+    """
+    Class for storing TCR-seq information and calculating useful properties.
+    """
+    def __init__(self, df) -> None:
+        self.data = df
+
+    def __repr__(self) -> str:
+        return f"TCR repertoire of size {len(self.data)}"
+
+    def to_df(self) -> pd.DataFrame:
+        """Convert to pandas DataFrame
+        """
+        return self.data

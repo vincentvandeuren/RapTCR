8268b48c6bbe2ebcbf0de6df45cf8f814fea468e
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-24 15:21:48 +0100

    formatting

--- raptcr/analysis.py
+++ raptcr/analysis.py
@@ -2,12 +2,15 @@ import pandas as pd
 from abc import ABC
 from functools import cached_property
 
+
 class TcrCollection(ABC):
     def __init__(self, df) -> None:
         # assert all required columns are present
         required_cols = ["v_call", "j_call", "junction_aa"]
         if not set(required_cols).issubset(df.columns):
-            raise ValueError(f"Input dataframe requires following columns: {required_cols}")
+            raise ValueError(
+                f"Input dataframe requires following columns: {required_cols}"
+            )
 
         self.data = df
 
@@ -22,8 +25,7 @@ class TcrCollection(ABC):
         return self.cdr3s[n]
 
     def to_df(self) -> pd.DataFrame:
-        """Convert to pandas DataFrame
-        """
+        """Convert to pandas DataFrame"""
         return self.data
 
     @cached_property
@@ -32,15 +34,17 @@ class TcrCollection(ABC):
 
 
 class Repertoire(TcrCollection):
-    """Class for storing TCR-seq information and calculating useful properties.
-    """
+    """Class for storing TCR-seq information and calculating useful properties."""
+
     def __repr__(self) -> str:
         return f"TCR repertoire of size {len(self.data)}"
+
 
 class Cluster(TcrCollection):
     """
     Class for storing Cluster-level clonotype information, and calculating its
     useful properties.
     """
+
     def __repr__(self) -> str:
         return f"TCR cluster of size {len(self.data)}"

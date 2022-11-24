08016455e99954887063a54d7b257874f977a941
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-24 15:26:46 +0100

    fix imports

--- raptcr/hashing.py
+++ raptcr/hashing.py
@@ -1,8 +1,6 @@
-from bitarray import bitarray
 from bitarray.util import urandom, zeros
-from functools import lru_cache, cached_property
+from functools import lru_cache
 import numpy as np
-import pandas as pd
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from typing import Union

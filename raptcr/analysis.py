4badb6169f04534f9e3e487f038957021bd85510
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-24 10:59:30 +0100

    refactor classes

--- raptcr/analysis.py
+++ raptcr/analysis.py
@@ -1,16 +1,35 @@
 import pandas as pd
+from abc import ABC
 
-class Repertoire():
-    """
-    Class for storing TCR-seq information and calculating useful properties.
-    """
+class TcrCollection(ABC):
     def __init__(self, df) -> None:
+        # assert all required columns are present
+        required_cols = ["v_call", "j_call", "junction_aa"]
+        if not set(required_cols).issubset(df.columns):
+            raise ValueError(f"Input dataframe requires following columns: {required_cols}")
+
         self.data = df
+        super().__init__()
 
     def __repr__(self) -> str:
-        return f"TCR repertoire of size {len(self.data)}"
+        return f"TCR collection of size {len(self.data)}"
 
     def to_df(self) -> pd.DataFrame:
         """Convert to pandas DataFrame
         """
         return self.data
+
+
+class Repertoire(TcrCollection):
+    """Class for storing TCR-seq information and calculating useful properties.
+    """
+    def __repr__(self) -> str:
+        return f"TCR repertoire of size {len(self.data)}"
+
+class Cluster(TcrCollection):
+    """
+    Class for storing Cluster-level clonotype information, and calculating its
+    useful properties.
+    """
+    def __repr__(self) -> str:
+        return f"TCR cluster of size {len(self.data)}"

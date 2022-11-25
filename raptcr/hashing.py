a97c246ae8fd89ee7dfbf8239d24d9e65a81b1e2
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-25 11:14:44 +0100

    remove unused import

--- raptcr/hashing.py
+++ raptcr/hashing.py
@@ -1,7 +1,6 @@
 from bitarray.util import urandom, zeros
 from functools import lru_cache
 import numpy as np
-import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from typing import Union
 

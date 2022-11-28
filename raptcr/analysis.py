676f852a22734885dda16c8d802c2c4daafd2e3d
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2022-11-28 15:30:51 +0100

    add sampling method and __len__

--- raptcr/analysis.py
+++ raptcr/analysis.py
@@ -24,9 +24,28 @@ class TcrCollection(ABC):
     def __getitem__(self, n):
         return self.cdr3s[n]
 
+    def __len__(self):
+        return len(self.data)
+
     def to_df(self) -> pd.DataFrame:
         """Convert to pandas DataFrame"""
         return self.data
+
+    def sample(self, n:int, weight_col:str=None):
+        """
+        Randomly sample n TCRs.
+
+        Parameters
+        ----------
+        n : int
+            Number of TCRs to sample.
+        weight_col : str or array-like, optional
+            If not passed, each TCR has equal probability to be picked. If an
+            array is passed, array values determine the probabilty of TCR at
+            that index to be picked. If a string is passed, the corresponding
+            column from self.data will be used as weight.
+        """
+        return self.__class__(df=self.data.sample(n, weights=weight_col))
 
     @cached_property
     def cdr3s(self):

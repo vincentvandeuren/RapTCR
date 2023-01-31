3c752f91833177ab92166e845b903b9007a355e3
Vincent Van Deuren <vincentvandeuren@uantwerpen.be>
2023-01-31 10:01:24 +0100

    add clustered rep

--- raptcr/analysis.py
+++ raptcr/analysis.py
@@ -2,11 +2,12 @@ import pandas as pd
 from abc import ABC
 from functools import cached_property
 
+from .tools import profile_matrix, motif_from_profile
 
 class TcrCollection(ABC):
     def __init__(self, df) -> None:
         # assert all required columns are present
-        required_cols = ["v_call", "j_call", "junction_aa"]
+        required_cols = self.required_cols if hasattr(self,"required_cols") else ["v_call", "j_call", "junction_aa"]
         if not set(required_cols).issubset(df.columns):
             raise ValueError(
                 f"Input dataframe requires following columns: {required_cols}"
@@ -67,3 +68,45 @@ class Cluster(TcrCollection):
 
     def __repr__(self) -> str:
         return f"TCR cluster of size {len(self.data)}"
+
+
+    def motif(self, method="standard", cutoff=0.7) -> str:
+        return motif_from_profile(profile_matrix(self.cdr3s), method, cutoff)
+
+
+class ClusteredRepertoire(TcrCollection):
+    """
+    Class for storing clustered clonotype information.
+    """
+    def __init__(self, df) -> None:
+        self.cluster_ids = df.index.get_level_values(level="cluster").unique().to_list()
+        super().__init__(df)
+
+    def __repr__(self) -> str:
+        if not "-1" in self.cluster_ids:
+            return f"Clustered TCR repertoire of size {len(self.data)} containing {len(self.cluster_ids)} clusters."
+        else:
+            num_unclustered_sequences = len(self.data.xs("-1", level="cluster"))
+            return f"Clustered TCR repertoire of size {len(self.data)} containing {len(self.cluster_ids)-1} clusters and {num_unclustered_sequences} unclustered sequences."
+
+    @classmethod
+    def from_clustcr_result(cls, df, clusteringresult):
+        df_merged = pd.merge(left=df, right=clusteringresult.clusters_df, how="left")
+        df_merged["cluster"] = df_merged["cluster"].fillna(-1).astype(int).astype(str)
+        df_merged=df_merged.reset_index().set_index(["cluster", "index"])
+        return cls(df_merged)
+
+    def __iter__(self):
+        for i in self.cluster_ids:
+            if i == "-1":
+                yield Repertoire(self.data.xs(i, level=0))
+            else:
+                yield Cluster(self.data.xs(i, level=0))
+
+    def iter_clusters(self):
+        for i in self.cluster_ids:
+            if i != "-1":
+                yield Cluster(self.data.xs(i, level=0))
+
+    def __len__(self):
+        return len(self.data)
